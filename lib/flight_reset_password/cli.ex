4202cc3fcff5ae4433e7eda6934bac5a27d0c753
shun <shun@getto.systems>
2017-09-09 00:26:07 +0900

    fix config

--- lib/flight_reset_password/cli.ex
+++ lib/flight_reset_password/cli.ex
@@ -2,32 +2,46 @@ defmodule FlightResetPassword.CLI do
   def main(arguments) do
     {_opts, args, _} = OptionParser.parse(arguments)
 
-    data = parse_data()
+    data = parse_data("FLIGHT_DATA")
+    #credential = parse_data("FLIGHT_CREDENTIAL")
+
+    Application.put_env(:flight_reset_password, FlightResetPassword.Mailer,
+      adapter: Bamboo.SMTPAdapter,
+      server: System.get_env("SMTP_SERVER"),
+      port: System.get_env("SMTP_PORT"),
+      username: System.get_env("SMTP_USER"),
+      password: System.get_env("SMTP_PASSWORD"),
+      tls: :if_available, # can be `:always` or `:never`
+      ssl: false, # can be `true`
+      retries: 1
+    )
 
     case args do
       ["send-email" | _] ->
-        case data["email"] do
-          nil -> "no email" |> puts_result(104)
-          "" ->  "no email" |> puts_result(104)
-          email ->
-            case data["token"] do
-              nil -> "no token" |> puts_result(104)
-              "" ->  "no token" |> puts_result(104)
-              token ->
-                FlightResetPassword.Email.reset_password_text_email(email,token)
-                |> FlightResetPassword.Mailer.deliver_now
-                %{"message" => "ok"} |> puts_result
-            end
+        case {data["email"] || "",data["token"] || ""} do
+          {"",""} -> "no email and token" |> puts_result(104)
+          {"",_}  -> "no email" |> puts_result(104)
+          {_,""}  -> "no token" |> puts_result(104)
+          {email,token}  ->
+            FlightResetPassword.Email.reset_password_text_email(email,token)
+            |> FlightResetPassword.Mailer.deliver_now
+            %{"message" => "ok"} |> puts_result
         end
+      _ -> "unknown command: #{arguments |> inspect}" |> puts_error
     end
   end
 
-  defp parse_data do
-    System.get_env("FLIGHT_DATA")
-    |> Poison.decode!
+  defp parse_data(key) do
+    System.get_env(key)
     |> case do
       nil -> %{}
-      data -> data
+      raw ->
+        raw
+        |> Poison.decode!
+        |> case do
+          nil -> %{}
+          data -> data
+        end
     end
   end
 
